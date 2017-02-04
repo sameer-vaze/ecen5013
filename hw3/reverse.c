a93ccab1533897eeaf44df42d23b388780896bdd
Sameer Vaze <sameer.vaze@colorado.edu>
2017-02-04 10:47:02 -0700

    Adding hw3 file

---
+++ hw3/reverse.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdint.h>
+
+char str1[17] = "This is a string.";
+char str2[18] = "some NUMmbers12345";
+char str3[30] = "Does it reverse \n\0\t correctly?";
+
+char reverse(char * str, int length);	//function declaration
+
+char reverse(char * str, int length)	//function definition
+{
+	unsigned int len = (unsigned int) length;	//typecast to unsigned integer type
+	if(len == 0 || str == NULL)	//checks for conditions that cause failure
+	{
+		return 1;
+	}
+	else if(len == 1)	//checks if only one bytes is passed
+	{
+		return 0;	//does not reverse and returns success
+	}
+	else	//if conditions are valid for reverse
+	{
+		char *start, *end, temp;
+		start = str;
+		end = str + len-1;
+		if((len%2) == 0)	//if length is even
+		{
+			do
+			{
+				temp  = *start;
+				*start = *end;
+				*end = temp;
+				start ++;
+				end--;
+			}while(start + 1 != end && start < end);	//check for middle
+		}
+		else	//if length is odd
+		{
+			do
+			{
+				temp  = *start;
+				*start = *end;
+				*end = temp;
+				start ++;
+				end--;
+			}while(start != end);	//check for middle
+		}
+		return 0;	//returns success
+	}
+}
+
+void main()
+{
+	char res = reverse(str1, 17);
+	for(int i = 0;i < 17;i++)
+	{
+		printf("%c",str1[i]);
+	}
+	printf("\n");
+	res = reverse(str2, 18);
+	for(int i = 0;i < 18;i++)
+	{
+		printf("%c",str2[i]);
+	}
+	printf("\n");
+	res = reverse(str1, 30);
+	for(int i = 0;i < 30;i++)
+	{
+		printf("%c",str3[i]);
+	}
+}
